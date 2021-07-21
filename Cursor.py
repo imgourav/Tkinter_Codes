6de3caf902bb231392eb213255e2b0d51bfad032
Gourav Sharma <gs3150007@gmail.com>
2021-07-21 09:47:28 +0530

    Update Cursor.py

--- Cursor.py
+++ Cursor.py
@@ -6,3 +6,29 @@ root.geometry("300x300")
 Button(root,text="Hover Me!",cursor="hand2").pack(pady=100)
 
 root.mainloop()
+
+#You Can also use below given stylings in place of "hand2"
+"""
+list = [
+    "arrow",
+    "circle",
+    "clock",
+    "cross",
+    "dotbox",
+    "exchange",
+    "fleur",
+    "heart",
+    "man",
+    "mouse",
+    "pirate",
+    "plus",
+    "shuttle",
+    "sizing",
+    "spider",
+    "spraycan",
+    "star",
+    "target",
+    "tcross",
+    "trek",
+]
+"""
